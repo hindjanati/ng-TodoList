b176a89d9aa5c2294e79ed5da24dce6f127c354f
Hindjanati <100366241+Hindjanati@users.noreply.github.com>
2022-11-03 16:42:31 +0100

    added css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'yellow-400' : '#E6C825',
+      'yellow-100' : '#FEFDFC'
+    }
   },
   plugins: [],
 }
